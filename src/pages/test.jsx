7d225caff999e58c5929fe1f95301f7213d00e5a
annasfajarramadhan2@gmail.com <annasfajarramadhan2@gmail.com>
2025-04-07 22:06:16 +0700

    add docker

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,8 +1,8 @@
 const Test = () => {
     return(
-        <>
+        <div className="flex justify-center items-center border">
             <h1>connect</h1>
-        </>
+        </div>
     )
 }
 
